e1e3a2d7d23442757caadfd13fc5f530e8c729c0
franavils <aviles110@hotmail.com>
2016-05-04 11:37:28 +0200

    First Commit

---
+++ Assets/CharacterController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+    Rigidbody2D rb;
+    public float Speed;
+
+	// Use this for initialization
+	void Start () {
+
+        rb = GetComponent<Rigidbody2D>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float currentY = rb.velocity.y;
+
+        //Horizontal movement
+        float moveHorizontal = (Input.GetAxis("Horizontal"));
+        rb.velocity = new Vector2(moveHorizontal * Speed, currentY);
+
+        //Flip        
+        if (rb.velocity.x > 0)
+        {
+            
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+
+            
+        }
+        else if (rb.velocity.x < 0)
+        {
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x)*-1, transform.localScale.y, transform.localScale.z);
+            
+        }
+    }
+}
---
+++ Assets/ShadowManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShadowManager : MonoBehaviour {
+
+    SpriteRenderer TopShadow;
+    GameObject TopLine;
+    bool topDicovered;
+    SpriteRenderer MidShadow;
+    GameObject MidLine;
+    bool midDicovered;
+    GameObject Player;
+
+    void Start()
+    {
+        Player = GameObject.Find("Player");
+        MidLine = GameObject.Find("MidLine");
+        MidShadow = GameObject.Find("MidShadow").GetComponent<SpriteRenderer>();
+        TopLine = GameObject.Find("TopLine");
+        TopShadow = GameObject.Find("TopShadow").GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (!midDicovered && Player.transform.position.y > MidLine.transform.position.y)
+        {
+            print("DISCOVER" +Time.time);
+            midDicovered = true;
+            StartCoroutine("Discover", MidShadow);
+        }
+//        print("midDicovered " + midDicovered);
+//        print("Player.transform.position.y "+ Player.transform.position.y+ " vs.  MidLine.transform.position.y " + MidLine.transform.position.y);
+        if (!topDicovered && Player.transform.position.y > TopLine.transform.position.y)
+        {
+            topDicovered = true;
+            StartCoroutine("Discover", TopShadow);
+        }
+    }
+
+    IEnumerator Discover (SpriteRenderer shadow)
+    {
+        print("Triggered");
+        float alpha = 1f;
+        while(alpha > 0f)
+        {
+            alpha -= Time.deltaTime;
+            shadow.color = new Color(shadow.color.r, shadow.color.g, shadow.color.b, alpha);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
