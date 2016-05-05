c8210e5903969ce7524a08071c0bed87efa2fd8a
franavils <aviles110@hotmail.com>
2016-05-05 16:58:36 +0200

    Old Woman

---
+++ Assets/MoveLeftRight.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveLeftRight : MonoBehaviour {
+
+    public int PaseoLenght;
+    SpriteRenderer sprite;
+    public float SlowDown;
+
+    public float pingpongValue;
+    float currentX;
+
+	// Use this for initialization
+	void Start () {
+        sprite = GetComponent<SpriteRenderer>();
+        currentX = GetComponent<Transform>().position.x;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        pingpongValue = Mathf.PingPong(Time.time/SlowDown, PaseoLenght);
+        transform.position = new Vector2 (currentX + pingpongValue, transform.position.y);
+
+        if (pingpongValue < 0.01 && !sprite.flipX)
+        {
+            sprite.flipX = !sprite.flipX;
+        }
+
+        if (pingpongValue > PaseoLenght- 0.01 && sprite.flipX)
+        {
+            sprite.flipX = !sprite.flipX;
+        }
+
+        
+	
+	}
+}
